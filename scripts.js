920b48d400a05d6faf2e6590d9d2bf3100e1dcd1
mgdeange <156377035+mgdeange@users.noreply.github.com>
2025-06-09 19:28:29 -0700

    AAAAAAAAAAAAAAAAAAAA

--- scripts.js
+++ scripts.js
@@ -1,4 +1,4 @@
-const buttons = document.querySelectorAll('.expand-button');
+  const buttons = document.querySelectorAll('.expand-button');
 let openButton = null;
 
 buttons.forEach(button => {
@@ -30,24 +30,31 @@ function showContent() {
   const container = document.getElementById("gallery-content");
   const caption = document.getElementById("caption");
   const currentItem = images[index];
-  
-  // Clear the container
+
+  // Clear previous content
   container.innerHTML = '';
-  
+
   if (currentItem.type === "img") {
     const img = document.createElement("img");
     img.src = currentItem.src;
     img.alt = "Project Image";
+    img.style.maxWidth = "100%";
+    img.style.maxHeight = "100%";
+    img.style.cursor = "pointer";
     img.onclick = openModal;
     container.appendChild(img);
   } else if (currentItem.type === "iframe") {
     const iframe = document.createElement("iframe");
     iframe.src = currentItem.src;
+    iframe.width = "560";
+    iframe.height = "315";
+    iframe.frameBorder = "0";
+    iframe.allow =
+      "accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share";
     iframe.allowFullscreen = true;
-    iframe.allow = "accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture";
     container.appendChild(iframe);
   }
-  
+
   caption.textContent = currentItem.caption;
 }
 
